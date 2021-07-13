b449b1013c5350e2f8319ef64ade7d8e208be3eb
RobertNiklasBock <robert-niklas.bock@tu-ilmenau.de>
2021-07-13 20:12:52 +0200

    Organized test cases.

---
+++ tests/safe_switch_test.py
@@ -0,0 +1,89 @@
+import pytest
+
+from mehr import safe_switch
+from mehr.exceptions import \
+    CaseAfterDefaultException, \
+    SameCaseException, \
+    DoubleDefaultException, \
+    NoDefaultException
+
+
+def test_normaluse():
+    i = 4
+    j = 0
+
+    with safe_switch(i) as (case, default, break_):
+        if case(2):
+            j = 1
+        if case(4):
+            j = 2
+        if case(10):
+            j = 5
+            break_()
+        if case(25):
+            j = 12.5
+        if default():
+            j = -1
+
+    assert(j == 5)
+
+def test_caseafterdefault_does_raise():
+    with pytest.raises(CaseAfterDefaultException):
+        with safe_switch(42) as (case, default, break_):
+            if case(1):
+                pass
+            if case(2):
+                pass
+            if default():
+                pass
+            if case(3):
+                pass
+
+
+def test_doubledefault_does_raise():
+    with pytest.raises(DoubleDefaultException):
+        with safe_switch(42) as (case, default, break_):
+            if case(1):
+                pass
+            if case(2):
+                pass
+            if default():
+                pass
+            if default():
+                pass
+
+def test_samecase_does_raise():
+    with pytest.raises(SameCaseException):
+        with safe_switch(2) as (case, default, break_):
+            if case(1):
+                pass
+            if case(2):
+                pass
+            if case(2):
+                pass
+            if default():
+                pass
+
+
+def test_samecase_dominates_nodefault():
+    with pytest.raises(SameCaseException):
+        with safe_switch(52) as (case, default, break_):
+            if case(1):
+                pass
+            if case(2):
+                pass
+            if case(2):
+                pass
+
+def test_samecase_raises_even_after_break():
+    with pytest.raises(SameCaseException):
+        with safe_switch(52) as (case, default, break_):
+            if case(1):
+                pass
+            if case(2):
+                pass
+            if case(3):
+                break_()
+            if case(2):
+                pass
+    
--- tests/switch_test.py
+++ tests/switch_test.py
@@ -1,9 +1,16 @@
-from mehr import switch, safe_switch
+import pytest
 
-def test_normal_use():
+from mehr import switch
+from mehr.exceptions import \
+    CaseAfterDefaultException, \
+    SameCaseException, \
+    DoubleDefaultException, \
+    NoDefaultException
+
+
+def test_normaluse():
     i = 4
     j = 0
-
 
     with switch(i) as (case, default, break_):
         if case(2):
@@ -14,7 +21,76 @@ def test_normal_use():
             j = 5
             break_()
         if case(25):
-            print(f"case 25")
             j = 12.5
+        if default():
+            j = -1
 
     assert(j == 5)
+
+def test_caseafterdefault_does_raise():
+    with pytest.raises(CaseAfterDefaultException):
+        with switch(42) as (case, default, break_):
+            if case(1):
+                pass
+            if case(2):
+                pass
+            if default():
+                pass
+            if case(3):
+                pass
+
+
+def test_doubledefault_does_raise():
+    with pytest.raises(DoubleDefaultException):
+        with switch(42) as (case, default, break_):
+            if case(1):
+                pass
+            if case(2):
+                pass
+            if default():
+                pass
+            if default():
+                pass
+
+def test_samecase_doesnt_raise():
+    j = 0
+    with switch(2) as (case, default, break_):
+        if case(1):
+            pass
+        if case(2):
+            j = 2
+        if case(2):
+            j = 3
+        if default():
+            pass
+
+    assert(j == 3)
+
+def test_samecase_doesnt_raise_after_break():
+    j = 0
+    with switch(2) as (case, default, break_):
+        if case(1):
+            pass
+        if case(2):
+            j = 2
+        if case(3):
+            j = 42
+            break_()
+        if case(2):
+            j = 3
+        if default():
+            pass
+
+    assert(j == 42)
+
+def test_unexpected_nameerror():
+    with switch(52) as (case, default, break_):
+        if case(1):
+            pass
+        if case(2):
+            j = 2
+        if case(2):
+            j = 3
+    
+    with pytest.raises(NameError):
+        x = (j == 0)
